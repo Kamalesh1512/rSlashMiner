4268e1222e9f150b9332ebbb8505e7a691975ee3
kamalesh1512 <kmgowda1512@gmail.com>
2025-04-20 16:20:11 +0530

    Day-9 small fix

--- src/app/api/agents/[agentId]/update-config/route.ts
+++ src/app/api/agents/[agentId]/update-config/route.ts
@@ -4,8 +4,14 @@ import { db } from "@/lib/db";
 import { auth } from "@/lib/auth";
 import { agents } from "@/lib/db/schema";
 
+interface configProps{
+  params:Promise<{
+    agentId:string,
+    }>
+}
 
-export async function POST(req: NextRequest) {
+
+export async function POST(req: NextRequest,{params}:configProps) {
   try {
     const session = await auth();
     if (!session || !session.user?.id) {
@@ -14,10 +20,8 @@ export async function POST(req: NextRequest) {
 
     const { configuration, isActive } = await req.json();
 
-    // Extract the agentId from the URL
-    const url = new URL(req.url);
-    const segments = url.pathname.split("/");
-    const agentId = segments[segments.length - 2]; // assumes route is .../agents/[agentId]/update-config
+    const {agentId} = await params
+    
 
     await db
       .update(agents)
