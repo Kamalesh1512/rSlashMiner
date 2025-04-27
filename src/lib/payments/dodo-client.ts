b8730855278daf70a7bea218b12b47581acc3cec
kamalesh1512 <kmgowda1512@gmail.com>
2025-04-27 13:19:24 +0530

    Day-12

--- src/lib/payments/dodo-client.ts
+++ src/lib/payments/dodo-client.ts
@@ -7,5 +7,5 @@
 import DodoPayments from 'dodopayments';
 
 export const dodoClient = new DodoPayments({
-bearerToken: process.env['DODO_PAYMENTS_API_KEY'],
+bearerToken: process.env.DODO_PAYMENTS_API_KEY,
 });
